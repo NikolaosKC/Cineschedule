955584e301c1ed8a762998792770309854c06f43
NikolaosKC <149090247+NikolaosKC@users.noreply.github.com>
2025-06-18 17:20:51 +0300

    INTRODUCED LIGHTMODE + JQuery + updated Bootstrap version

--- JS/CinescheduleOverall.js
+++ JS/CinescheduleOverall.js
@@ -4,3 +4,18 @@ function formValidation() {
     alert(name); */
     alert("Form submitted!");
 }
+
+$(document).ready(function(){
+    //$("p").css("border", "3px solid red");
+    $("#deliverplz").load("CinescheduleNavbar.html");
+});
+
+$(document).ready(function(){
+    $("#lightmodeAppend").click(function() {
+        $("header").append('<link href="CSS/CinescheduleLightmode.css" type="text/css" rel="stylesheet">');
+    });
+    $("#lightmodeToggle").click(function() {
+        $('body, hr, h1, h2, h3, p, table, th, td').toggleClass('lightmode');
+        $('.navButton, .input, :submit, .movieFestivalGrid, .teamMembers, #lightmodeToggle').toggleClass('lightmode');
+    });
+});
